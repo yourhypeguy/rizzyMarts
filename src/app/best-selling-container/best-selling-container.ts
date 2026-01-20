37c6c726fec9ae3a354d90414ffce34b09329ba0
yourhypeguy <105666787+yourhypeguy@users.noreply.github.com>
2026-01-20 14:35:05 +0100

    Initial commit

--- src/app/best-selling-container/best-selling-container.ts
+++ src/app/best-selling-container/best-selling-container.ts
@@ -1,18 +1,19 @@
-import { Component, signal, OnInit, OnDestroy, NgZone } from '@angular/core';
-
+import { Component, signal, OnInit, OnDestroy, NgZone, Inject } from '@angular/core';
+import { CommonModule,isPlatformBrowser } from '@angular/common';  
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-best-selling-container',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './best-selling-container.html',
   styleUrl: './best-selling-container.css',
 })
 export class BestSellingContainer implements OnInit, OnDestroy {
   //list of products
 products = [
-  {image: '', price: 29.99},
-  {image: '', price: 40.99},
-  {image: '', price: 89.99},
+  {image: '/assets/25kg_kingsOil.png', name: '25l Kings-Oil', price: 70000},
+  {image: '/assets/royal-stalion-rice.png', name: '10kg Rice "Royal-Stallion"', price: 40000},
+  {image: '/assets/rose-carla-tissue-single.png', name: 'Bag of Rose Tissue', price: 1000},
 ];
 
 //track which product is showing
@@ -21,22 +22,26 @@ currentIndex = signal(0);
 //interval reference (IMPORTANT FOR STOPPING MEMORY LEAKS)
 intervalId: any;
 
-constructor(private ngZone: NgZone) {}
+constructor(private ngZone: NgZone, @Inject(PLATFORM_ID) private platformId: Object) {}
 
 ngOnInit() {
   this.startAutoSlide();
 }
 
-ngOnDestroy() { 
-  clearInterval(this.intervalId);
+ngOnDestroy() {
+  if (this.intervalId) {
+    clearInterval(this.intervalId);
+  }
 }
 
 //automatically change product every 3 seconds
 startAutoSlide() {
-  this.intervalId= setInterval(() => {
-    this.nextProduct();
-  }, 3000);
-}
+    this.ngZone.runOutsideAngular(() => {
+      this.intervalId = setInterval(() => {
+        this.ngZone.run(() => this.nextProduct());
+      }, 3000);
+    });
+  }
 
 //move to next product
 nextProduct() {
@@ -47,4 +52,6 @@ this.currentIndex.update(i => (i + 1) % this.products.length);
 prevProduct() {
   this.currentIndex.update(i => (i - 1 + this.products.length) % this.products.length); 
 }
+
+
 }
